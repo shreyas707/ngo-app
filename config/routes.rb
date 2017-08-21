8b3c5298c8aededbdd302c9c0bcc39bc7d67d416
siddharth <siddharth.setlur@gmail.com>
2017-08-21 18:52:16 +0530

    website creation

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :websites
   resources :educators
   resources :learners
   resources :organizations
---
+++ test/system/websites_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class WebsitesTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit websites_url
+  #
+  #   assert_selector "h1", text: "Website"
+  # end
+end
