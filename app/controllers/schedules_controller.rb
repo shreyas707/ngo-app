dea86b941ed4910ac09f3732bae4c916a71bad40
siddharth <siddharth.setlur@gmail.com>
2017-09-02 18:27:43 +0530

    adding 5 and half hours to the utc time before saving

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -40,6 +40,7 @@ class SchedulesController < ApplicationController
 			@schedule.educator_id = current_user.educator.id
 			@schedule.confirmed = true
 			@schedule.date_time_of_class =  DateTime.strptime(params[:schedule][:date_time_of_class], '%m/%e/%Y %I:%M %p')
+			@schedule.date_time_of_class = @schedule.date_time_of_class + 5.hours + 30.minutes
 			if @schedule.save
 			redirect_to schedules_path, notice: "Great the meeting is set! You can view it in your schedule. Come back then and we'll connect you!"
 			# NotificationMailer.schedule_confirmation(@schedule).deliver!
